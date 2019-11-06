3495899ab4b0cbb9f71830dacef56a58cd782172
Peter Sulzen <psulzen@gmail.com>
2019-11-06 15:23:32 -0600

    changes

---
+++ Data.py
@@ -0,0 +1,9 @@
+# Dependencies
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+csv_path = "Resources/city_data.csv"
+city_data_df = pd.read_csv(csv_path)
+df.to_html('WeatherPy.html')
